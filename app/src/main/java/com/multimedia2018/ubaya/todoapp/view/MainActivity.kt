e692b901e531c41c1c269df3fb1d8d6ff1d8e86a
GeraldiWinson <s16081801@student.ubaya.ac.id>
2021-10-14 12:20:32 +0700

    1.
    Week 8 finished, BUT with bugs
    
    "Tidak bisa jalan program karena urusan kapt (?)"
    "Masalah dengan Dao-nya"

--- app/src/main/java/com/multimedia2018/ubaya/todoapp/view/MainActivity.kt
+++ app/src/main/java/com/multimedia2018/ubaya/todoapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.multimedia2018.ubaya.todoapp
+package com.multimedia2018.ubaya.todoapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.multimedia2018.ubaya.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
